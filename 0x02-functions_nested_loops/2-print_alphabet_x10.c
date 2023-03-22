09577f21cfe2b2afdb5567d7ac191e6058c9f39e
@chrissy-ai <christinetsotetsi2@gmail.com>
2023-03-22 09:00:27 -0700

    Functions and Nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 *
-	 * Description: This function prints the alphabet 10 times in lowercase
-	 * followed by a new line. It uses a nested loop to iterate through each
-	 * letter of the alphabet and print it to the console, then repeats the
-	 * process 10 times.
-	 */
-	void print_alphabet_x10(void)
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ *
+ * Description: This function prints the alphabet 10 times in lowercase
+ * followed by a new line. It uses a nested loop to iterate through each
+ * letter of the alphabet and print it to the console, then repeats the
+ * process 10 times.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+
+	i = 0;
+
+
+	while (i < 10)
 	{
-		char ch;
-		int i;
-
-
-		i = 0;
-
-
-		while (i < 10)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
+	}
 }
