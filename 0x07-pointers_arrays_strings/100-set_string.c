f1c6d1dcdcd6152e1c0180f1fe94eeecd1f6d576
@chrissy-ai <christinetsotetsi2@gmail.com>
2023-04-04 13:22:08 -0700

    Even more pointers arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - updates a pointer to a character with a new value
+ * @s:  pointer to pointer to character that will be updated
+ * @to:  pointer to character containing the new value
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
